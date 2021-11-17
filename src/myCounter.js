d40e9aea6cd64df05c54cbc79116ba6b75bb4fbd
brucealberto <brucealberto2@gmail.com.br>
2021-11-16 22:14:33 -0300

    lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -21,7 +21,7 @@ const myCounter = () => {
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (var counter2 = 2; counter2 < 3; counter2 += 1) {
-      myArray.push(2,3);
+      myArray.push(2, 3);
     }
   }
   return myArray;
